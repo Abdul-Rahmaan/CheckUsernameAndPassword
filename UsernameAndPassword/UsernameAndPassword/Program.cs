f4a82d79c7fac774e4a44e5359fd3ac358e33201
Abdur Rehman <abdurrehmanmughal1@gmail.com>
2023-07-20 11:46:16 +0500

    feat: check username and password

---
+++ UsernameAndPassword/UsernameAndPassword/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsernameAndPassword
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string username, password;
+
+            username = "abdurrehman555@gmail.com";
+            password = "pass555@";
+
+            Console.WriteLine("enter the username : ");
+
+            string user = Console.ReadLine();
+
+            Console.WriteLine("enter the password : ");
+
+            string pass = Console.ReadLine();
+
+            if (user == username && pass == password)
+            {
+                Console.WriteLine("login successfull");
+            }
+            else
+            {
+                Console.WriteLine("login unsuccessfull");
+            }
+        }
+
+    }
+}
